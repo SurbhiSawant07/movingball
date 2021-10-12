51457c5dbd75fa4faf89325900fb784cf912f609
SurbhiSawant07 <92374563+SurbhiSawant07@users.noreply.github.com>
2021-10-12 17:58:33 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+
+
+
+
+function setup() {
+  createCanvas(400,400);
+var box = createSprite(200,200,20,20)
+box.velocityX = 2
+
+}
+
+function draw() 
+{
+  background(30);
+drawSprites()
+}
+
+
+
+
